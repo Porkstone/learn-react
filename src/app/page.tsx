df0c0ddfba407c70f4dc8177fcc1946fb1aff8fc
Charlie Barker <charlie@hallville.net>
2024-06-03 09:41:41 +0100

    Updated content and some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/8eab33b1-1fe8-4a6b-9c2a-7a02fcd912f8-1fisai.png",
+  "https://utfs.io/f/8eab33b1-1fe8-4a6b-9c2a-7a02fcd912f8-1fisai.png",
+  "https://utfs.io/f/8eab33b1-1fe8-4a6b-9c2a-7a02fcd912f8-1fisai.png",
+  "https://utfs.io/f/8eab33b1-1fe8-4a6b-9c2a-7a02fcd912f8-1fisai.png",
+];
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+    url: url
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Zorro WebUI
+    <main className="">
+      <div className="flex flex-wrap gap-4 p-4">
+        Zorro WebUI
+        {mockImages.map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} />
+        </div>
+        ))}
+        </div>
     </main>
   );
 }
