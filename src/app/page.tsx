c23444806ae4436cc3caf44e3fcfaaa5d65d475b
Charlie <charlie@hallville.net>
2024-06-03 16:19:35 +0100

    Got WebUI talking to the vercel postgres database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/8eab33b1-1fe8-4a6b-9c2a-7a02fcd912f8-1fisai.png",
@@ -11,13 +12,21 @@ const mockImages = mockUrls.map((url, index) => ({
     url: url
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+   const posts = await db.query.posts.findMany();
+   //console.log(posts);
+
+ 
   return (
     <main className="">
+      Zorro WebUI*
       <div className="flex flex-wrap gap-4 p-4">
-        Zorro WebUI
-        {mockImages.map((image) => (
-        <div key={image.id} className="w-48">
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48">
           <img src={image.url} />
         </div>
         ))}
