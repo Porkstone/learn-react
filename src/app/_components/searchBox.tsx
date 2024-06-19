2a6ea6d8d936121b318773d525aed564bd769286
Charlie <charlie@hallville.net>
2024-06-19 13:49:07 +0100

    Moved results to below search box to impove UX on mobile

--- src/app/_components/searchBox.tsx
+++ src/app/_components/searchBox.tsx
@@ -53,53 +53,62 @@ function SearchResults(props: searchProps) {
                                             <div className='text-lg'> 
                                               <a href="https://google.co.uk" target="_self">{hotel.entity_name}</a> 
                                             </div>Region {hotel.hierarchy} - Id: {hotel.entity_id}</div>)
-
+  if(encodedString === '')
+    return (<div>...</div>)
+  
   return (
+      
     <div>
       <div className='text-xl'>Hotels</div>
       {hotelCards}
     </div>
   )
+  
+
 }
 function SearchBox() {
     const [searchText, setSearchText] = useState('');
     const [startDate, setStartDate] = useState(new Date().toISOString().split('T')[0]);
     
   return (<div>
-  <div className="flex gap-4 items-center justify-center border-b border-violet-500">
-    <div className="flex">
-      <div className=" flex flex-wrap items-center text-violet-700 py-2 gap-4">
-        <div className="flex flex-col">
-          <label className="text-sm" htmlFor="hotel">Hotel Name</label>
-          <input className="appearance-none text-xl border rounded w-72  py-2 px-3 text-violet-700  leading-tight"
-            type="text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Hotel Name" />
-        </div>
-        <div className="flex flex-col">
-          <label className="text-sm" htmlFor="startDate">Start Date</label>
-          <input className="appearance-none text-xl border rounded w-48  py-2 px-3 text-violet-700  leading-tight" value={startDate} type="date" id="startDate" name="startDate" onChange={(e) => setStartDate(e.target.value)} />  
-        </div>
-        <div className="flex flex-col">
-          <label className="text-sm" htmlFor="noNights">Number of Nights</label>
-          <NumberSelector defaultValue={7} />
-        </div>
-        <div className="flex flex-col">
-          <label className="text-sm" htmlFor="noPeople">Number of People</label>
-          <NumberSelector defaultValue={2} />
-        </div>
-        <div className="flex flex-col pt-5">
-          <button className="text-xl border rounded py-2 px-3 w-48 md:w-28 text-white bg-violet-500 leading-tight" type="submit">Search</button>
-        </div>
+    <div className="flex gap-4 items-center justify-center border-b border-violet-500">
+      <div className="flex">
+        <div className=" flex flex-wrap items-center text-violet-700 py-2 gap-4">
+          <div className="flex flex-col">
+            <label className="text-sm" htmlFor="hotel">Hotel Name</label>
+            <input className="appearance-none text-xl border rounded w-72  py-2 px-3 text-violet-700  leading-tight"
+              type="text"
+              autocorrect="off"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              placeholder="Hotel Name" />
+          </div>
+          <div className="flex flex-col">
+            <div>
+              <SearchResults searchText={searchText} />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm" htmlFor="startDate">Start Date</label>
+            <input className="appearance-none text-xl border rounded w-48  py-2 px-3 text-violet-700  leading-tight" value={startDate} type="date" id="startDate" name="startDate" onChange={(e) => setStartDate(e.target.value)} />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm" htmlFor="noNights">Number of Nights</label>
+            <NumberSelector defaultValue={7} />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm" htmlFor="noPeople">Number of People</label>
+            <NumberSelector defaultValue={2} />
+          </div>
+          <div className="flex flex-col pt-5">
+            <button className="text-xl border rounded py-2 px-3 w-48 md:w-28 text-white bg-violet-500 leading-tight" type="submit">Search</button>
+          </div>
 
+        </div>
       </div>
-    </div>
     
   </div>
-  <div>
-      <SearchResults searchText={searchText} />
-    </div>
+  
   </div>)
 }
 
